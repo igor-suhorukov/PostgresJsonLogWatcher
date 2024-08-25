eafc740a4266eeca440523c3a8dcbcde60ba1778
igor-suhorukov <igor-suhorukov@users.noreply.github.com>
2024-08-25 03:17:58 +0000

    Deploying to gh-pages from  @ c1d9aeef4726a40bc02f1e37ddb0703564f99419 🚀

--- testapidocs/package-search-index.js
+++ testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.isuhorukov.log.watcher"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.isuhorukov.log.watcher"},{"l":"com.github.isuhorukov.log.watcher.steps"}];updateSearchResults();
